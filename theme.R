a6e7c2f933a1ca99aa7d040afb00578198a3b81b
wisecj <charlottewise8@gmail.com>
2020-06-28 16:18:47 +0100

    initial theme and visualisation

---
+++ theme.R
@@ -0,0 +1,16 @@
+
+custom_theme <- function() {
+  theme(plot.title = element_text(family="Times", face="bold", size=20),
+        plot.subtitle = element_text(family = 'Times', size = 10),
+        axis.text = element_text(family = 'Times', size = 10),
+        panel.border     = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        # show axes
+        axis.line      = element_line(colour = "black"),
+        # match legend key to panel.background
+        legend.key       = element_blank(),
+        # simple, black and white strips
+        strip.background = element_rect(fill = "white"))
+}
